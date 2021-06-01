e69bfe31bb48466186531cbcfa6b18b629cf096a
robinrobusa <84471527+robinrobusa@users.noreply.github.com>
2021-05-31 19:27:05 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,17 +1,14 @@
 def buildApp() {
   echo 'Building the application using groovy...'
-  bat "mvn clean"
 }
 
 def testApp() {
   echo 'Testing the application using groovy...'
-  bat "mvn test"
 }
 
 def deployApp() {
   echo 'Deploying the application using groovy...'
   echo 'Deploying versiob ${params.VERSION}'
-  
 }
 
 return this
